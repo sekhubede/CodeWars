d2a9df122056e31aec06c4f3ad7dd630a08015fe
sekhubede <msekhubede@gmail.com>
2024-10-21 08:59:50 +0200

    Create Challenge.DescendingOrder.cs

---
+++ CodeWars/Challenges/Challenge.DescendingOrder.cs
@@ -0,0 +1,22 @@
+﻿namespace CodeWars.Challenges
+{
+    public static partial class Challenge
+    {
+        public static int DescendingOrder(int num)
+        {
+            char[] digits = num.ToString().ToCharArray();
+
+            Array.Sort(digits);
+            Array.Reverse(digits);
+
+            string sortedNumStr = new string(digits);
+
+            return int.Parse(sortedNumStr);
+        }
+
+        public static int DescendingOrderLinq(int num)
+        {
+            return int.Parse(string.Concat(num.ToString().OrderByDescending(x => x)));
+        }
+    }
+}
