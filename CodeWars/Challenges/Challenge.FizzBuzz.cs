ca8c549a5232266192d11bb6fcb8fd3520a69772
sekhubede <msekhubede@gmail.com>
2024-10-21 08:59:52 +0200

    Create Challenge.FizzBuzz.cs

---
+++ CodeWars/Challenges/Challenge.FizzBuzz.cs
@@ -0,0 +1,19 @@
+﻿namespace CodeWars.Challenges
+{
+    public static partial class Challenge
+    {
+        public static string Play(int number) =>
+       number switch
+       {
+           _ when number.IsDivisibleBy(15)
+               => "FizzBuzz",
+           _ when number.IsDivisibleBy(3)
+               => "Fizz",
+           _ when number.IsDivisibleBy(5)
+               => "Buzz",
+           _ => number.ToString()
+       };
+
+        private static bool IsDivisibleBy(this int i, int divisibleBy) => i % divisibleBy == 0;
+    }
+}
