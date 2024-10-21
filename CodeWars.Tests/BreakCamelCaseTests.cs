2091508604201af72542d6fcd7e80e8baecdf94d
sekhubede <msekhubede@gmail.com>
2024-10-21 13:06:48 +0200

    Add BreakCamelCase challenge

---
+++ CodeWars.Tests/BreakCamelCaseTests.cs
@@ -0,0 +1,44 @@
+﻿using CodeWars.Challenges;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Tests
+{
+    public class BreakCamelCaseTests
+    {
+        [Theory]
+        [InlineData("camelCasing", "camel Casing")]
+        [InlineData("camelCasingTest", "camel Casing Test")]
+        [InlineData("indentifier", "indentifier")]
+        [InlineData("", "")]
+        public void BreakCamelCase_ReturnCamelCasingUsingSpaceBetweenWords(string inputString, string expected)
+        {
+            // Arrange
+
+            // Act
+            string actual = Challenge.BreakCamelCase(inputString);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("camelCasing", "camel Casing")]
+        [InlineData("camelCasingTest", "camel Casing Test")]
+        [InlineData("indentifier", "indentifier")]
+        [InlineData("", "")]
+        public void BreakCamelCaseLinq_ReturnCamelCasingUsingSpaceBetweenWords(string inputString, string expected)
+        {
+            // Arrange
+
+            // Act
+            string actual = Challenge.BreakCamelCaseLinq(inputString);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ CodeWars/Challenges/Challenge.BreakCamelCase.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using System.Linq;
+
+namespace CodeWars.Challenges
+{
+    public static partial class Challenge
+    {
+        public static string BreakCamelCase(string str)
+        {
+            StringBuilder result = new StringBuilder();
+
+            foreach (char c in str)
+            {
+                // Check if the character is uppercase
+                if (char.IsUpper(c))
+                {
+                    // Add a space before the uppercase letter
+                    result.Append(' ');
+                }
+
+                // Add the character itself
+                result.Append(c);
+            }
+
+            return result.ToString();
+        }
+
+        public static string BreakCamelCaseLinq(string str)
+        {
+            return string.Concat(str.Select(c => Char.IsUpper(c) ? " " + c : c.ToString()));
+        }
+    }
+}
