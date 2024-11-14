927f397976d7a067ceec64797825d33f4d720a14
sekhubede <msekhubede@gmail.com>
2024-11-14 13:57:49 +0200

    Add IsPrime challenge file

---
+++ CodeWars.Tests/IsNumberPrimeTests.cs
@@ -0,0 +1,30 @@
+﻿using CodeWars.Challenges;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Tests
+{
+    public class IsNumberPrimeTests
+    {
+        [Theory]
+        [InlineData(0, false)]
+        [InlineData(1, false)]
+        [InlineData(2, true)]
+        public void IsNumberPrime_ReturnTrueIfNumberPrimeOtherwiseFalse(int number, bool expected)
+        {
+            // Arrange
+
+
+            // Act
+            bool actual = Challenge.IsPrime(number);
+
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ CodeWars/Challenges/Challenge.IsNumberPrime.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Challenges
+{
+    public static partial class Challenge
+    {
+        public static bool IsPrime(int n)
+        {
+            // Edge case: numbers <= 1 are not prime
+            if (n <= 1) return false;
+
+            // Special case: 2 is prime
+            if (n == 2) return true;
+
+            // Even numbers greater than 2 are not prime
+            if (n % 2 == 0) return false;
+
+            // Check only odd numbers up to the square root of n
+            int limit = (int)Math.Sqrt(n);
+            for (int i = 3; i <= limit; i += 2)
+            {
+                if (n % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            // If no divisors found, n is prime
+            return true;
+        }
+    }
+}
