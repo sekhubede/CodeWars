7f4ef94a8ab5c60bef5166cad05acbf79f4e12ef
sekhubede <msekhubede@gmail.com>
2024-11-14 12:55:47 +0200

    Add ClassicHelloWorld challenge script file

---
+++ CodeWars.Tests/ClassicHelloWorldTests.cs
@@ -0,0 +1,28 @@
+﻿using CodeWars.Challenges;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Tests
+{
+    public class ClassicHelloWorldTests
+    {
+        [Theory]
+        [InlineData(new string[] {"Greetings from Javatlacati"}, "Hello World!\r\n")]
+        [InlineData(new string[] { "parameter1", "parameter2", "parametern" }, "Hello World!\r\n")]
+        public void ClassicHelloWorld_PrintHelloWorld(string[] inputString, string expected)
+        {
+            // Arrange: Capture the console output
+            var output = new StringWriter();
+            Console.SetOut(output);
+
+            // Act: Call the method
+            Challenge.ClassicHelloWorld(inputString);
+
+            // Assert
+            Assert.Equal(expected, output.ToString());
+        }
+    }
+}
