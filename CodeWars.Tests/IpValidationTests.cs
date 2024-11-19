47cb56e59f23103a3417711c80bfe270104c4b5f
sekhubede <msekhubede@gmail.com>
2024-11-19 11:44:30 +0200

    Add IPValidation challenge

---
+++ CodeWars.Tests/IpValidationTests.cs
@@ -0,0 +1,33 @@
+﻿using CodeWars.Challenges;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Tests
+{
+    public class IpValidationTests
+    {
+        [Theory]
+        [InlineData("0.0.0.0", true)]
+        [InlineData("12.255.56.1", true)]
+        [InlineData("137.255.156.100", true)]
+        [InlineData("", false)]
+        [InlineData("abc.def.ghi.jkl", false)]
+        [InlineData("12.34.56", false)]
+        [InlineData("12.34.56.78sf", false)]
+        [InlineData("123.045.067.089", false)]
+        [InlineData("12.34.56 .1", false)]
+        public void IsValidIp_ReturnsTrueIfValidIPv4AddressInDotDecimalFormat(string ipAddress, bool expected)
+        {
+            // Arrange
+
+            // Act
+            bool actual = Challenge.IsValidIp(ipAddress);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ CodeWars/Challenges/Challenge.IpValidation.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Reflection.Metadata.Ecma335;
+
+namespace CodeWars.Challenges
+{
+    public static partial class Challenge
+    {
+        public static bool IsValidIp(string ipAddres)
+        {
+            // Check if there are any spaces anywhere inside the IP address
+            if (ipAddres.Contains(' '))
+                return false;
+
+            // Split the string using '.' as the delimiter
+            var octets = ipAddres.Split('.');
+
+            // Must have exactly 4 parts
+            if (octets.Length != 4)
+                return false;
+
+            foreach (var octet in octets)
+            {
+                // Check if part is empty or contains invalid characters
+                if (string.IsNullOrEmpty(octet) || !int.TryParse(octet, out int num) || octet.Length > 1 && octet[0] == '0')
+                    return false;
+
+                // Check if the number is in range (0 to 255)
+                if (num < 0 || num > 255)
+                    return false;
+            }
+
+            return true;
+        }
+
+
+        public static bool IsValidIPv4Regex(string ip)
+        {
+            string pattern = @"^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$";
+            return Regex.IsMatch(ip, pattern);
+        }
+    }
+}
