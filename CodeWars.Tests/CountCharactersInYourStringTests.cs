07529a5ff4261fe038544d509a442bcd13c0cd42
sekhubede <msekhubede@gmail.com>
2024-11-20 12:55:19 +0200

    Add CountCharactersInYourString challenge

---
+++ CodeWars.Tests/CountCharactersInYourStringTests.cs
@@ -0,0 +1,29 @@
+﻿using CodeWars.Challenges;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Tests
+{
+    public class CountCharactersInYourStringTests
+    {
+        [Theory]
+        [InlineData("", 0)]                // Empty string
+        [InlineData("a", 1)]               // Single character
+        [InlineData("aba", 2)]             // Repeating characters
+        [InlineData("aaabbc", 3)]          // Multiple characters
+        [InlineData("abcdefghijklmnopqrstuvwxyz", 26)] // All unique characters
+        public void GetCharacterCount_returns_a_dictionary_with_all_characters_and_their_occurances(string input, int expected)
+        {
+            // Arrange (Given)
+
+            // Act (When)
+            var actual = Challenge.GetCharacterCount(input);
+
+            // Assert (Then)
+            Assert.Equal(expected, actual.Count);
+        }
+    }
+}
---
+++ CodeWars/Challenges/Challenge.CountCharactersInYourString.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Challenges
+{
+    public static partial class Challenge
+    {
+        public static Dictionary<char, int> GetCharacterCount(string str)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                return new Dictionary<char, int>();
+            }
+
+            var characterCount = new Dictionary<char, int>();
+
+            foreach (char c in str)
+            {
+                if (!characterCount.ContainsKey(c))
+                {
+                    characterCount.Add(c, 1);
+                    continue;
+                }
+
+                characterCount[c] += 1;
+            }
+
+            return characterCount;
+        }
+
+        public static Dictionary<char, int> GetCharacterCountEx(string str)
+        {
+            var characterCount = new Dictionary<char, int>();
+
+            foreach (char c in str)
+            {
+                if (characterCount.TryGetValue(c, out int count))
+                {
+                    characterCount[c] = count + 1;
+                }
+                else
+                {
+                    characterCount[c] = 1;
+                }
+            }
+
+            return characterCount;
+        }
+    }
+}
