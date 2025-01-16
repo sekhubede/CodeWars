52f5f3d8b62953f82ad336d5bf1661ec3b8b9372
sekhubede <msekhubede@gmail.com>
2025-01-16 13:09:21 +0200

    Add ReversedStrings Challenge

---
+++ CodeWars.Tests/ReversedStringsTests.cs
@@ -0,0 +1,27 @@
+﻿using CodeWars.Challenges;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Tests
+{
+    public class ReversedStringsTests
+    {
+        [Theory]
+        [InlineData("world", "dlrow")]
+        [InlineData("word", "drow")]
+        public void ReversedStrings_reverses_the_string_passed(string str, string expected)
+        {
+            // Arrange
+
+            // Act
+            string actual = Challenge.ReversedStrings(str);
+
+            // Assert
+            actual.Should().Be(expected);
+        }
+    }
+}
---
+++ CodeWars/Challenges/Challenge.ReversedStrings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Challenges
+{
+    public static partial class Challenge
+    {
+        public static string ReversedStrings(string str) => new string(str.Reverse().ToArray());
+    }
+}
