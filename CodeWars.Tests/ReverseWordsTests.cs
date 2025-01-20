0049eaa635f1f29addf4173f01931b68ffdd29df
sekhubede <msekhubede@gmail.com>
2025-01-20 13:03:00 +0200

    Add ReverseWords challenge file

---
+++ CodeWars.Tests/ReverseWordsTests.cs
@@ -0,0 +1,42 @@
+﻿using CodeWars.Challenges;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Tests
+{
+    public class ReverseWordsTests
+    {
+        [Theory]
+        [InlineData("This is an example!", "sihT si na !elpmaxe")]
+        [InlineData("double  spaces", "elbuod  secaps")]
+        public void ReverWords_reverses_each_word_in_the_string(string str, string expected)
+        {
+            // Arrange
+
+            // Act
+            string actual = Challenge.ReverseWords(str);
+
+            // Assert
+            actual.Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData("This is an example!", "sihT si na !elpmaxe")]
+        [InlineData("double  spaces", "elbuod  secaps")]
+        public void ReverWords_Linq_reverses_each_word_in_the_string(string str, string expected)
+        {
+            // Arrange
+
+            // Act
+            string actual = Challenge.ReverseWords_Linq(str);
+
+            // Assert
+            actual.Should().Be(expected);
+        }
+
+    }
+}
---
+++ CodeWars/Challenges/Challenge.ReverseWords.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Challenges
+{
+    public partial class Challenge
+    {
+        public static string ReverseWords(string str)
+        {
+            var newString = str.Split(' ');
+
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < newString.Length; i++)
+            {
+                var revWord = new string(newString[i].Reverse().ToArray());
+
+                sb.Append(revWord).Append(" ");
+
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string ReverseWords_Linq(string str) => 
+            string.Join(" ", str.Split(' ')
+                .Select(word => new string(word.Reverse().ToArray())));
+    }
+}
