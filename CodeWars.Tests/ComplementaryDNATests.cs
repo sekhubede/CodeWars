eba468292d104f22e91ad0dd4143205cf5a91e7b
sekhubede <msekhubede@gmail.com>
2025-01-21 13:51:41 +0200

    Add Complementary DNA challenge file

---
+++ CodeWars.Tests/ComplementaryDNATests.cs
@@ -0,0 +1,49 @@
+﻿using CodeWars.Challenges;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Tests
+{
+    public class ComplementaryDNATests
+    {
+        [Theory]
+        [InlineData("AAAA", "TTTT")]
+        [InlineData("ATTGC", "TAACG")]
+        [InlineData("GTAT", "CATA")]
+        [InlineData("AAGG", "TTCC")]
+        [InlineData("CGCG", "GCGC")]
+        [InlineData("GTATCGATCGATCGATCGATTATATTTTCGACGAGATTTAAATATATATATATACGAGAGAATACAGATAGACAGATTA", "CATAGCTAGCTAGCTAGCTAATATAAAAGCTGCTCTAAATTTATATATATATATGCTCTCTTATGTCTATCTGTCTAAT")]
+        public void MakeComplement_return_the_other_complementary_DNA_strand(string dna, string expected)
+        {
+            // Arrange
+
+            // Act
+            var actual = Challenge.MakeComplement(dna);
+
+            // Assert
+            actual.Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData("AAAA", "TTTT")]
+        [InlineData("ATTGC", "TAACG")]
+        [InlineData("GTAT", "CATA")]
+        [InlineData("AAGG", "TTCC")]
+        [InlineData("CGCG", "GCGC")]
+        [InlineData("GTATCGATCGATCGATCGATTATATTTTCGACGAGATTTAAATATATATATATACGAGAGAATACAGATAGACAGATTA", "CATAGCTAGCTAGCTAGCTAATATAAAAGCTGCTCTAAATTTATATATATATATGCTCTCTTATGTCTATCTGTCTAAT")]
+        public void MakeComplement_Linq_return_the_other_complementary_DNA_strand(string dna, string expected)
+        {
+            // Arrange
+
+            // Act
+            var actual = Challenge.MakeComplement_Linq(dna);
+
+            // Assert
+            actual.Should().Be(expected);
+        }
+    }
+}
---
+++ CodeWars/Challenges/Challenge.ComplementaryDNA.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Challenges
+{
+    public static partial class Challenge
+    {
+        public static string MakeComplement(string dna)
+        {
+            var sb = new StringBuilder();
+
+            foreach(var nucleotide in dna)
+            {
+                switch(nucleotide)
+                {
+                    case 'A':
+                        sb.Append('T');
+                        break;
+                    case 'T':
+                        sb.Append('A');
+                        break;
+                    case 'C':
+                        sb.Append('G');
+                        break;
+                    case 'G':
+                        sb.Append('C');
+                        break;
+                    default:
+                        throw new ArgumentException("Invalid DNA");
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        public static string MakeComplement_Linq(string dna)=> new string(dna.Select(c => c switch
+            {
+                'A' => 'T',
+                'T' => 'A',
+                'C' => 'G',
+                'G' => 'C',
+                _ => throw new ArgumentException("Invalid DNA")
+            }).ToArray());
+    }
+}
