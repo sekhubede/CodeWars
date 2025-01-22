a1bd4ab810cbac84dd4f0524b809814894d26df3
sekhubede <msekhubede@gmail.com>
2025-01-22 15:29:35 +0200

    Add challenge definition and failing test

---
+++ CodeWars.Tests/NumberOfPeopleInTheBusTests.cs
@@ -0,0 +1,34 @@
+﻿using CodeWars.Challenges;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Tests
+{
+    public class NumberOfPeopleInTheBusTests
+    {
+        [Theory]
+        [MemberData(nameof(GetTestData))]
+        public void Number_ShouldReturnCorrectNumberOfPeopleLeft(List<int[]> peopleListInOut, int expected)
+        {
+            // Arrange
+
+            // Act
+            var actual = Challenge.Number(peopleListInOut);
+
+            // Assert
+            actual.Should().Be(expected);
+        }
+
+        public static IEnumerable<object[]> GetTestData()
+        {
+            yield return new object[] { new List<int[]> { new int[] { 10, 0 }, new int[] { 3, 5 }, new int[] { 5, 8 } }, 5 };
+            yield return new object[] { new List<int[]> { new int[] { 3, 0 }, new int[] { 9, 1 }, new int[] { 4, 10 }, new int[] { 12, 2 }, new int[] { 6, 1 }, new int[] { 7, 10 } }, 17 };
+            yield return new object[] { new List<int[]> { new int[] { 3, 0 }, new int[] { 9, 1 }, new int[] { 4, 8 }, new int[] { 12, 2 }, new int[] { 6, 1 }, new int[] { 7, 8 } }, 21 };
+        }
+    }
+}
---
+++ CodeWars/Challenges/Challenge.NumberOfPeopleInTheBus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Challenges
+{
+    public static partial class Challenge
+    {
+        public static int Number(List<int[]> peopleListInOut) =>
+            throw new NotImplementedException();
+    }
+}
